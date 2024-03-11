f9dfc93bb80ee6569a49e882ef728bc7ba9b593b
fhash <fhash@DESKTOP-B4EG033.Home>
2024-03-11 22:01:57 +0300

    finished class

---
+++ projectJava113/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module projectJava113 {
+}
--- projectJava113/src/projectJava113/Movie.java
+++ projectJava113/src/projectJava113/Movie.java
@@ -7,6 +7,71 @@ public class Movie {
 	private String genre;
 	private double rating;
 	private String director;
+
+	
+	public Movie()
+	{
+		title="";
+		releaseYear=0;
+		genre="";
+		rating=0;
+		director="";
+	}
+
+
+	public Movie(String title, int releaseYear, String genre, double rating, String director) {
+		this.title = title;
+		this.releaseYear = releaseYear;
+		this.genre = genre;
+		this.rating = rating;
+		this.director = director;
+	}
+	
+	public String getDetails()
+	{
+		String str= "Movie [Title=" + title + ", releaseYear=" + releaseYear + ", genre=" + genre + ", rating=" + rating
+				+ ", director=" + director + "]";
+		return str;
+	}
+
+
+	public String getGenre() {
+		return genre;
+	}
+
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
