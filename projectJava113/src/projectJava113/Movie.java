9e5a2aad2be31f7b58ab1a75791623301622690a
fhash <fhash@DESKTOP-B4EG033.Home>
2024-03-11 20:54:36 +0300

    addded attributes for class movie

--- projectJava113/src/projectJava113/Movie.java
+++ projectJava113/src/projectJava113/Movie.java
@@ -2,4 +2,12 @@ package projectJava113;
 
 public class Movie {
 
+	private String title;
+	private int releaseYear;
+	private String genre;
+	private double rating;
+	private String director;
+	
+	
+	
 }
