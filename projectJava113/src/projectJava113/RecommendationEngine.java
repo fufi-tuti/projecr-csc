21dcbbdd6b1cc34f313c4d850bc08cef2949626c
misha <misha@LAPTOP-6N68CLKM>
2024-03-12 16:32:53 +0300

    made the abstract class

--- projectJava113/src/projectJava113/RecommendationEngine.java
+++ projectJava113/src/projectJava113/RecommendationEngine.java
@@ -1,5 +1,21 @@
 package projectJava113;
 
 public class abstract RecommendationEngine {
+	
+	public String [] recommendedMovies(User user); //will be overridden in subclasses
+	
+	public String getRecommendation(User user) { //retrieves recommendations as a string
+		
+		String [] moviesArray = recommendedMovies(user); //this will hold array of favorite movies
+		String str = "";  // will accumulate movies on this string
+		
+		for( int i = 0 ; i < moviesArray.length ; i++ ) { // a loop to accumulate movies on str
+			str += moviesArray[i]+"\n" //new line for each movie
+		}
+		
+		return str; //returning recommended movies as a string
+			
+		
+	}
 
 }
